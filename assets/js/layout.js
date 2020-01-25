6d5872fbc5f558fdd387972c6ffa0c8645a1384f
netiapaul <paul.netia@nadiaapp.com>
2020-01-25 10:42:46 +0300

    removed picture

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,4 +1,21 @@
 $(function () {
     $("#header").load("./partials/header.html");
     $("#footer").load("./partials/footer.html");
-    });
+});
+
+// $(function (){
+//     $(document).scroll(function(){
+//     var $nav = $("#header");
+//     $nav.toggleClass('scrolled',$(this).scrollTop() > $nav.height());
+//     }) ;
+// });
+
+$(function (){
+    $(window).on('scroll',function(){
+        if($(window).scrollTop()){
+            $('nav').addClass('scrolled')
+        }else{
+            $('nav').removeClass('scrolled')
+        }
+    })
+});
